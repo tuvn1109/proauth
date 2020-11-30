91765402f1c4864a30c0b2b5f1cb8f77384de19d
TU VN <nvtu1009@gmail.com>
2020-11-30 17:37:02 +0700

    17g30 30/11

--- app/Views/product/index.php
+++ app/Views/product/index.php
@@ -18,23 +18,163 @@
     </div>
 </section>
 
-
 <section id="main-product">
-    <div class="row  ">
-        <div class="col-md-6 col-12 d-flex justify-content-center">
-            sssss
+    <div class="row ">
+        <div class="col-md-6 col-12 d-flex  justify-content-center"
+             style="border: 1px solid red;">
+            <div class="img-pro">
+                <img src="http://proauth.com/logo/bestsell1.jpg"></div>
         </div>
         <div class="col-md-6 col-12 d-flex justify-content-center">
             <div class="row">
                 <div class="col-12">
-                    <div class="link-pro"></div>
-                    <div class="title-pro"><p>Fancy Cloud -Oversize Printed Tee - Up to 9 Kids </p></div>
+                    <div class="link-pro">Home > T-shirt</div>
+                </div>
+                <div class="col-12">
+                    <div class="title-pro"><span>Fancy Cloud -Oversize Printed Tee - Up to 9 Kids </span></div>
+                </div>
+                <div class="col-12">
                     <div class="price-pro"><p>$24.99 USD</p></div>
-                    <div class="sale-timeout-pro"><img src="https://s3-alpha-sig.figma.com/img/749b/abcf/e19afa554519859ce55ab2a65fd84809?Expires=1607299200&Signature=F39Ozy6bkYKW9NRk8BfbFd4CUf1Aa9AZz0zLamcQFSEBT8Z6JbqhMBxbM3iNwri86bRIJ1DKlxlEqQ91zsgoJLAw40gTGbf7KLWj~FZ8YjQ5GIO-vaunfaHec1z50b8944TXD9Uz2Xqb0Me9DqTqHrsTG1iPCuB1Df3FBPiciAcF8g2o~7oQGS~pgj2jWYXrZX1vEhykDtFsY6b7rJzwy05p5DvXQKAK26M067LICAnP9kq~b5Wlt-T3wy128FMLZ2kzEUfY0pMnMVp2gjskdfKV7CtZbaSHgQ-lCLdtqWNoJ0XF1pIZcQTjmpySd7pv6lg5oQL8~i2vP3j8UZlYBA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"></div>
+
+                </div>
+                <div class="col-12">
+                    <div class="sale-timeout-pro"><img
+                                src="https://s3-alpha-sig.figma.com/img/749b/abcf/e19afa554519859ce55ab2a65fd84809?Expires=1607299200&Signature=F39Ozy6bkYKW9NRk8BfbFd4CUf1Aa9AZz0zLamcQFSEBT8Z6JbqhMBxbM3iNwri86bRIJ1DKlxlEqQ91zsgoJLAw40gTGbf7KLWj~FZ8YjQ5GIO-vaunfaHec1z50b8944TXD9Uz2Xqb0Me9DqTqHrsTG1iPCuB1Df3FBPiciAcF8g2o~7oQGS~pgj2jWYXrZX1vEhykDtFsY6b7rJzwy05p5DvXQKAK26M067LICAnP9kq~b5Wlt-T3wy128FMLZ2kzEUfY0pMnMVp2gjskdfKV7CtZbaSHgQ-lCLdtqWNoJ0XF1pIZcQTjmpySd7pv6lg5oQL8~i2vP3j8UZlYBA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
+                    </div>
+                </div>
+                <div class="col-12">
                     <div class="express-pro"><p>ORDER CUT OFF FOR CHRISTMAS: Nov 25</p></div>
+                </div>
+                <div class="col-6 d-flex">
+                    <div class=""><img src="/logo/US-logo.png" style="height: 30px;width: 30px"></div>
+                    <div class="info-pro">Products are manufactured in<br><span>United States</span></div>
+
+                </div>
+                <div class="col-6 d-flex">
+                    <div class=""><img src="/logo/truck-logo.png" style="height: 30px;width: 30px"></div>
+                    <div class="info-pro">Products are manufactured in<br><span>Express by 29 Jan</span></div>
+
+                </div>
+                <div class="col-12">
                     <div class="title-discription-pro"><p>We design products for fan</p></div>
-                    <div class="discription-pro"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Curabitur gravida arcu ac tortor dignissim convallis aenean et. Dolor sit amet consectetur adipiscing elit ut aliquam purus.</p></div>
-                    <div class="tag-pro"><p>#T-shirt #T-shirt #T-shirt</p></div>
+                    <div class="discription-pro"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Curabitur gravida arcu ac tortor
+                            dignissim
+                            convallis aenean et. Dolor sit amet consectetur adipiscing elit ut aliquam purus.</p></div>
+                </div>
+                <div class="col-12  d-flex">
+                    <div><img src="/logo/tag-logo.png"
+                              style="height: 14.374929428100586px;width: 14.37868881225586px;"></div>
+                    <div class="tag d-flex">
+                        <div class="tag-text">#T-shirt</div>
+                        <div class="tag-text">#Men</div>
+                        <div class="tag-text">#Oversize</div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 col-12">
+        </div>
+        <div class="col-md-6 col-12">
+            <div class="row">
+                <div class="col-3">Size Guide</div>
+                <div class="col-3">Delivery & Return</div>
+                <div class="col-3">Ask a Question</div>
+            </div>
+
+
+            <div class="row mt-1">
+                <div class="col-12">
+                    <div id="div-choose">
+                        <div class="div-choose-type">
+                            <div class="col-12 title-type-pro"><span>Choose a product type</span></div>
+                            <div class="col-12 list-type-pro">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="item">
+                                            <div class="thumb"><img src="/logo/tshirt-type.jpg" class="img-fluid"></div>
+                                            <div class="title d-flex justify-content-center">xxxx</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="item">
+                                            <div class="thumb"><img src="/logo/tshirt-type.jpg" class="img-fluid"></div>
+                                            <div class="title d-flex justify-content-center">xxxx</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="item ">
+                                            <div class="thumb"><img src="/logo/tshirt-type.jpg" class="img-fluid"></div>
+                                            <div class="title d-flex justify-content-center">xxxx</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div id="div-choose-color">
+                            <div class="col-12 title-type-pro"><span>Color</span></div>
+                            <div class="col-12 list-color-pro ">
+                                <div class="row">
+                                    <div class="col-1 ">
+                                        <div class="item-color cl-white"></div>
+                                    </div>
+                                    <div class="col-1 ">
+                                        <div class="item-color cl-orange"></div>
+                                    </div>
+                                    <div class="col-1 ">
+                                        <div class="item-color cl-blue"></div>
+                                    </div>
+                                    <div class="col-1 ">
+                                        <div class="item-color cl-green"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="div-choose-size">
+                            <div class="col-12 title-type-pro"><span>Size</span></div>
+                            <div class="col-12 list-size-pro">
+                                <div class="row">
+                                    <div class="col-12  d-flex">
+                                        <div class="size"><span>S</span></div>
+                                        <div class="size"><span>SM</span></div>
+                                        <div class="size"><span>M</span></div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div id="div-preview">
+                            <div class="col-12 d-flex justify-content-center">
+                                <button type="button" id="btn-prevew"><span>PREVIEW</span></button>
+                            </div>
+                        </div>
+
+
+                        <div id="div-note">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-12"><span style="color: red">*</span> Please make sure that all the options you have chosen are correct before clicking on the "Add To Cart" button.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    
+                    <div id="div-addcart"></div>
+
+
+
                 </div>
             </div>
         </div>
